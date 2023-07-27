62db45df1cf915ef1a8f7bb67c581f6390bd3ae0
Müfit Sevinçli <msmufitmufit@gmail.com>
2023-07-27 20:18:36 +0300

    mantine uygulandı card tasarımı iyileştirildi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-//import logo from "./logo.svg";
+import { Button, Input, Textarea, Container, Grid, Stack } from "@mantine/core";
 import "./App.css";
 import Card from "./COMPENENTS/Card";
 import { useState } from "react";
+import { setSelectionRange } from "@testing-library/user-event/dist/utils";
 
 let arr = [
   {
@@ -19,50 +20,61 @@ let arr = [
     title: "dağ 3",
     par: "acıklama 3",
   },
+  {
+    id: 4,
+    title: "dağ 4",
+    par: "acıklama 4",
+  },
 ];
 
 //const kare = (sayi) => sayi * sayi;
 
 const App = () => {
+  const [title, setTitle] = useState("");
+  const [paragraf, setParagraph] = useState("");
+  const [list, setList] = useState(arr);
   const [lesson, setLesson] = useState(11);
+  const click = () => {
+    setTitle("");
+    setParagraph("");
+    const copyList = [...list];
+    copyList.push({
+      id: 5,
+      title: title,
+      par: paragraf,
+    });
+    setList(copyList);
+  };
   return (
-    <div>
-      <h1>Başlık</h1>
-      <h2>Ders {lesson}</h2>
-      <button
-        onClick={() => {
-          setLesson(lesson + 1);
-        }}
-      >
-        Ders arttır
-      </button>
-      <button
-        onClick={() => {
-          setLesson(lesson - 1);
-        }}
-      >
-        azalt
-      </button>
-      <button
-        onClick={() => {
-          setLesson(100);
-        }}
-      >
-        Ders 100
-      </button>
-      <p>Başlangıç</p>
-      <div className="Cards">
-        {arr.map(({ title, par }, i) => (
-          <Card
-            key={`ìndex ${i}`}
-            title={title}
-            par={par}
-            lesson={lesson}
-            index={i}
+    <Container>
+      <h1>Kart oluşturma programı</h1>
+      <Stack>
+        <Input.Wrapper label="Başlık">
+          <Input
+            placeholder="Başlık yazınız"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
+        </Input.Wrapper>
+        <Textarea
+          placeholder="Paragraf yazınız"
+          label="Paragraf"
+          value={paragraf}
+          onChange={(e) => setParagraph(e.target.value)}
+        />
+        <Button variant="outline" onClick={click}>
+          Kart oluştur
+        </Button>
+      </Stack>
+      <h2>Kartlar:</h2>
+      <Grid>
+        {list.map(({ title, par }, i) => (
+          <Grid.Col span={4} key={`ìndex ${i}`}>
+            <Card title={title} par={par} lesson={lesson} a={i} />
+          </Grid.Col>
         ))}
-      </div>
-    </div>
+      </Grid>
+    </Container>
   );
 };
 
